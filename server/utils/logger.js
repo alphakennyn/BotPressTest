1df121f6f44b36aa4d6719360de32187a347af71
alphakennyn <alphakennyn@gmail.com>
2022-03-12 00:22:11 -0500

    chore: init inhouse log tool

---
+++ server/utils/logger.js
@@ -0,0 +1,11 @@
+const logger = (text) => {
+    
+    // if (process.env.NODE_ENV === 'development') {
+        console.log(text)
+        return;
+    // }
+
+    // todo log with winston
+}
+
+module.exports = logger
