db0bd0c1b5599612065b2d39d921a40b1a116cc5
alphakennyn <alphakennyn@gmail.com>
2022-03-13 16:04:56 -0400

    feat: add loading

---
+++ app/src/components/Loading/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../../logo.png";
+
+const Loading = () => (
+  <div id="loading-icon">
+    <img src={logo} className="rotate" alt="loading-logo" />
+  </div>
+);
+
+export default Loading;
