2dd80412e3810bbd3691c84230f3dcd6e8741b75
alphakennyn <alphakennyn@gmail.com>
2022-03-14 22:16:09 -0400

    chore: remove deprecated server folder

--- server/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const getDirectory = require('./getDirectory');
-
-const router = express.Router();
-
-router.get('/readDirectory', getDirectory);
-
-module.exports = router;
